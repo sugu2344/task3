dbf69b79b5ea57c1096b99670502c916031e94b4
sugu2344 <suganesh234@gmail.com>
2024-11-21 23:53:06 +0530

    card names renamed  and added query selector

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log("hello everyone");
+const gridDisplay = document.getElementById("#grid");
+const resultDisplay = document.querySelector("#score");
+const cardArray = [
+  {
+    name: "2spade",
+    img: "public/2spade",
+  },
+  {
+    name: "3spade",
+    img: "public/3spade",
+  },
+  {
+    name: "4spade",
+    img: "public/4spade",
+  },
+  {
+    name: "5spade",
+    img: "public/5spade",
+  },
+  {
+    name: "6spade",
+    img: "public/6spade",
+  },
+  {
+    name: "7spade",
+    img: "public/7spade",
+  },
+  {
+    name: "8spade",
+    img: "public/8spade",
+  },
+  {
+    name: "9spade",
+    img: "public/9spade",
+  },
+  {
+    name: "10spade",
+    img: "public/10spade",
+  },
+];
